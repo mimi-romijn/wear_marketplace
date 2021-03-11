aee44edee4e4671952a96049eb15c85f2d7c15fd
mimi-romijn <romijnmimi@gmail.com>
2021-03-11 17:11:54 +1100

    add scaffold

---
+++ db/migrate/20210311053951_create_listings.rb
@@ -0,0 +1,11 @@
+class CreateListings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :listings do |t|
+      t.string :title
+      t.text :description
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
